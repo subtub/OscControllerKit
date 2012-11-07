657da47fc0d459107db85cf478f89cbbc3dbc476
Tim Pulver <tim_pulver@gmx.de>
2012-11-07 14:15:40 +0100

    Starting to write a main class
    This should handle all reflection / general stuff..
    -> Unfinished

---
+++ OscController/src/de/fhpotsdam/io/OscControllerKit.java
@@ -0,0 +1,9 @@
+package de.fhpotsdam.io;
+
+import de.fhpotsdam.io.osc.wii.WiiOsc;
+import de.fhpotsdam.util.Reflection;
+
+public class OscControllerKit {
+	WiiOsc wiiOsc;
+	Reflection reflection;
+}
